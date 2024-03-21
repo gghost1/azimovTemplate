1293ec2a3a83d8cca03a144d9f4b00ebcf9e4105
GG_Host <gd.host@yandex.ru>
2024-03-21 08:02:47 +0300

    Simple caching implemented

--- src/main/java/com/example/azimovTemplate/AzimovTemplateApplication.java
+++ src/main/java/com/example/azimovTemplate/AzimovTemplateApplication.java
@@ -1,9 +1,13 @@
 package com.example.azimovTemplate;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableCaching
 public class AzimovTemplateApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/azimovTemplate/Services/UserModelDetailsService.java
+++ src/main/java/com/example/azimovTemplate/Services/UserModelDetailsService.java
@@ -4,22 +4,29 @@ import com.example.azimovTemplate.Models.User.UserModel;
 import com.example.azimovTemplate.Models.User.UserModelDetails;
 import com.example.azimovTemplate.Services.Reprositories.UserModelReprository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class UserModelDetailsService implements UserDetailsService {
 
     @Autowired
     private UserModelReprository reprository;
 
     @Override
+    @Cacheable(value = "users", key = "#username")
     public UserModelDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<UserModel> user = reprository.findByName(username);
+        System.out.println("load");
         return user.map(UserModelDetails::new).orElseThrow(() -> new UsernameNotFoundException(username + " not found"));
     }
+    @CachePut(value = "users", key = "#user.name")
     public void updateUser(UserModel user) throws Exception {
         Optional<UserModel> userOld = reprository.findByName(user.getName());
         if (userOld.isEmpty()) throw new Exception();
