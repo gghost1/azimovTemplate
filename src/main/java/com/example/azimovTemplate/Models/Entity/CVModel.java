901fdfb71475db46764da4e89e2bcb2bcf6f7566
GG_Host <gd.host@yandex.ru>
2024-06-15 23:12:06 +0300

    cv edited

--- src/main/java/com/example/azimovTemplate/Models/Entity/CVModel.java
+++ src/main/java/com/example/azimovTemplate/Models/Entity/CVModel.java
@@ -5,10 +5,13 @@ import lombok.Data;
 @Data
 public class CVModel {
     private int age;
+    private String description;
+    private String education;
     private String location;
     private String country;
     private boolean move;
     private boolean businessTrip;
     private String sheduel;
     private String prevJob;
+    private String skills;
 }
